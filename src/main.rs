7c30976440efb3f9bd5b2fd1621b052b2916edc1
luke-bhan <luke.bhan@vanderbilt.edu>
2020-04-30 00:53:51 -0500

    added cli hooks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::{App, load_yaml};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    match matches.subcommand_name() {
+        Some("init") => println!("test"),
+        _ => println!("fail")
+    }
 }
