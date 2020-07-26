6cbbe311c04801450d02644f4fcecd791243722c
luke.bhan@vanderbilt.edu <luke.bhan@vanderbilt.edu>
2020-07-26 15:28:50 -0500

    set init command to create objects and refs dirs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use clap::{App, load_yaml};
+use std::env;
+use std::path::Path;
+use std::fs;
 
-fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from(yaml).get_matches();
+pub fn init(path: &Path) {
+    let git_path = path.join(".git");
 
-    match matches.subcommand_name() {
-        Some("init") => println!("test"),
-        _ => println!("fail")
+    let dirs = vec!["objects", "refs"];
+    for name in dirs.iter() {
+        fs::create_dir_all(git_path.join(name)).unwrap();
     }
+    print!("Initialized empty lit repository in {}", git_path.display());
 }
+
+fn main() -> std::io::Result<()> {
+    // get command line argument for path
+    let args: Vec<String> = env::args().collect();
+
+    // if empty use working directory as default path
+    match args.get(1) {
+        Some(cmd) => {
+            if String::from("init") == cmd.to_string() {
+                match args.get(2) {
+                    Some(dir) => init(&Path::new(dir).canonicalize().unwrap()),
+                    None => init(&env::current_dir()?),
+                };
+            } else {
+                print!("No command named {}", cmd);
+            }
+        },
+        _ => print!("Please provide a command"),
+    };
+    Ok(())
+}
+
+
+    
+
+
+
